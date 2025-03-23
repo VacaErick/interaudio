3045ceef8b0823b2054d87b33591bc10a436cfcf
Vaca Erick <125614647+VacaErick@users.noreply.github.com>
2025-03-23 02:59:32 -0600

    modified:   pom.xml 	new file:   src/main/java/com/interAudio/InteraudioApplication.java 	modified:   src/main/resources/application.properties

---
+++ src/main/java/com/interAudio/InteraudioApplication.java
@@ -0,0 +1,9 @@
+package com.interAudio;
+
+import org.springframework.boot.SpringApplication;
+
+public class InteraudioApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InteraudioApplication.class, args);
+}
+}
